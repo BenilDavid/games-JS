90525ee93a8259614c6e3a24f3ad717ddd26dc17
BenilDavid <benil.26david@gmail.com>
2020-07-31 13:17:20 +0530

    third commit

--- Js/home.js
+++ Js/home.js
@@ -1,20 +1,40 @@
+//challenge age in days
+
 function ageInDays(){
     var year = prompt("what is the year you were born..?");
     var ageInDays = (2020 - year) * 365;
 
-    var h1 = document.createElement('h1');
+    var h3 = document.createElement('h3');
     var textanswer = document.createTextNode('you are '+ageInDays+' days old');
-    h1.appendChild(textanswer);
-    document.getElementById('flexbox-result').appendChild(h1);
+    h3.appendChild(textanswer);
+    document.getElementById('flexbox-result').appendChild(h3);
 }
 
 function reset(){
     document.getElementById("flexbox-result").remove();
 }
 
+//challenge generate cat
+
 function generatecat(){
     var image = document.createElement('img');
     image.src = 'img/cat.jpg';
     var div = document.getElementById('cat-gen');
     div.appendChild(image);
+}
+
+//challenge rock paper scissor
+
+function rpsgame(){
+    var botchoice,humanchoice;
+botchoice = numberToChoice(random());
+alert(botchoice);
+}
+
+function random(){
+   return Math.floor(Math.random() * 3);
+}
+
+function numberToChoice(number){
+ return ['rock','paper','scisssor'][number];
 }
